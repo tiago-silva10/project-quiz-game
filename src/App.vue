657d2a99f1bef2285ead36be1ac7c6f2b22a9d0e
Tiago do Nascimento Silva <tiago_silvaa10@outlook.com>
2023-12-19 14:36:16 -0300

    Criação do projeto quiz-game

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Bem Vindo!</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  max-width: 900px;
+}
+</style>
