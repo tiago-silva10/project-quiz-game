75689a9d1042f30082e3bad427d85a2b361e37a3
Tiago do Nascimento Silva <tiago_silvaa10@outlook.com>
2023-12-19 14:58:25 -0300

    criando pagina inicial

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
-  <h1>Bem Vindo!</h1>
+<div>
+  <h1>Welcome!</h1>
+  <input type="radio" name="options">
+  <label>Answer</label>
+  <br />
+  <input type="radio" name="options">
+  <label>Answer</label>
+  <br />
+  <button type="submit">Send</button>
+</div>
 </template>
 
 <script>
@@ -14,6 +23,31 @@ export default {
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  max-width: 900px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 60px auto;
+  max-width: 960px;
 }
+
+h1 {
+  margin-top: 40px;
+}
+
+input[type='radio']{
+  margin: 12px 4px;
+}
+
+button.send {
+  margin-top: 12px;
+  height: 40px;
+  min-width: 120px;
+  padding: 0 16px;
+  color: #fff;
+  background-color: #1867c0;
+  border: 1px solid #1867c0;
+  cursor: pointer;
+}
+
 </style>
